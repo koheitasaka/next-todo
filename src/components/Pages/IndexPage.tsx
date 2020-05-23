471964e0dd35b495f7c13c9697b0664411e4b66a
hirozak <shrak0224@gmail.com>
2020-05-23 16:18:58 +0900

    create form
    fixup! create form

--- src/components/Pages/IndexPage.tsx
+++ src/components/Pages/IndexPage.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
+import styled from '@emotion/styled';
 import AppTemplate from '@/components/Templates/AppTemplate';
+import Form from '@/components/Organisms/Form';
 
 const IndexPage: React.FC = () => {
   return (
     <AppTemplate>
-      <h1>hello</h1>
+      <Form />
     </AppTemplate>
   );
 };
