6f1e60648f634a1106c6a85b139285b71b1bc1f4
hirozak <shrak0224@gmail.com>
2020-05-25 01:26:18 +0900

    add useEffect

--- src/components/Pages/IndexPage.tsx
+++ src/components/Pages/IndexPage.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
-import styled from '@emotion/styled';
+import { connect } from 'react-redux';
+import { IStore, ThunkDispatch } from '@/modules/store';
 import AppTemplate from '@/components/Templates/AppTemplate';
 import Form from '@/components/Organisms/Form';
 import List from '@/components/Organisms/List';
 
-const IndexPage: React.FC = () => {
+const mapStateToProps = (store: IStore) => ({
+  todoList: store.todoList,
+});
+
+const mapDispatchToProps = (dispatch: ThunkDispatch) => ({});
+
+type IProps = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
+
+const IndexPage: React.FC<IProps> = ({ todoList }) => {
+  React.useEffect(() => console.log('first render'), []);
   return (
     <AppTemplate>
       <Form />
@@ -13,4 +24,4 @@ const IndexPage: React.FC = () => {
   );
 };
 
-export default IndexPage;
+export default connect(mapStateToProps, mapDispatchToProps)(IndexPage);
